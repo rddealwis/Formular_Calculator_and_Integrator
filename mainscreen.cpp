052573e9a531f1d768a9f7ea14cea628f529f91f
chintakawijetunga <chintaka.wijetunga@yahoo.com>
2015-07-01 19:37:11 +0530

    Button Click events
    Not completely implemented. continuous entries not supported yet

--- mainscreen.cpp
+++ mainscreen.cpp
@@ -12,3 +12,88 @@ MainScreen::~MainScreen()
 {
     delete ui;
 }
+
+void MainScreen::on_pbMultiply_clicked()
+{
+   ui->txtEditor->setText("*");
+}
+
+void MainScreen::on_pbZero_clicked()
+{
+   ui->txtEditor->setText("0");
+}
+
+void MainScreen::on_pbOne_clicked()
+{
+   ui->txtEditor->setText("1");
+}
+
+void MainScreen::on_pbTwo_clicked()
+{
+    ui->txtEditor->setText("2");
+}
+
+void MainScreen::on_pbThree_clicked()
+{
+    ui->txtEditor->setText("3");
+}
+
+void MainScreen::on_pbFour_clicked()
+{
+    ui->txtEditor->setText("4");
+}
+
+void MainScreen::on_pbFive_clicked()
+{
+    ui->txtEditor->setText("5");
+}
+
+void MainScreen::on_pbSix_clicked()
+{
+    ui->txtEditor->setText("6");
+}
+
+void MainScreen::on_pbSeven_clicked()
+{
+    ui->txtEditor->setText("7");
+}
+
+void MainScreen::on_pbEight_clicked()
+{
+    ui->txtEditor->setText("8");
+}
+
+void MainScreen::on_pbNine_clicked()
+{
+    ui->txtEditor->setText("9");
+}
+
+void MainScreen::on_pbDot_clicked()
+{
+    ui->txtEditor->setText(".");
+}
+
+void MainScreen::on_pbPlus_clicked()
+{
+    ui->txtEditor->setText("+");
+}
+
+void MainScreen::on_pbMinus_clicked()
+{
+    ui->txtEditor->setText("-");
+}
+
+void MainScreen::on_pbEqual_clicked()
+{
+    ui->txtEditor->setText("=");
+}
+
+void MainScreen::on_pbInverse_clicked()
+{
+    ui->txtEditor->setText("1/");
+}
+
+void MainScreen::on_pbPercentage_clicked()
+{
+    ui->txtEditor->setText("%");
+}
--- mainscreen.h
+++ mainscreen.h
@@ -15,6 +15,41 @@ public:
     explicit MainScreen(QWidget *parent = 0);
     ~MainScreen();
 
+private slots:
+    void on_pbMultiply_clicked();
+
+    void on_pbZero_clicked();
+
+    void on_pbOne_clicked();
+
+    void on_pbTwo_clicked();
+
+    void on_pbThree_clicked();
+
+    void on_pbFour_clicked();
+
+    void on_pbFive_clicked();
+
+    void on_pbSix_clicked();
+
+    void on_pbSeven_clicked();
+
+    void on_pbEight_clicked();
+
+    void on_pbNine_clicked();
+
+    void on_pbDot_clicked();
+
+    void on_pbPlus_clicked();
+
+    void on_pbMinus_clicked();
+
+    void on_pbEqual_clicked();
+
+    void on_pbInverse_clicked();
+
+    void on_pbPercentage_clicked();
+
 private:
     Ui::MainScreen *ui;
 };
